4be334d0beee139095cdfc8f2fe567d4e8d9387f
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-04 11:44:45 +0700

    Initial

---
+++ workshop-testing/src/main/java/org/tdd/workshoptesting/WorkshopTestingApplication.java
@@ -0,0 +1,13 @@
+package org.tdd.workshoptesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopTestingApplication.class, args);
+    }
+
+}
