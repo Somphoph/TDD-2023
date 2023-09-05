fa07574ab6433f937897e06d7be974e4b3ff6d4e
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-05 11:52:35 +0700

    Added UserControllerMockMvcTest for testing UserController.

---
+++ api/src/test/java/com/example/api/user/UserControllerMockMvcTest.java
@@ -0,0 +1,20 @@
+package com.example.api.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(UserController.class)
+class UserControllerMockMvcTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private UserService userService;
+    @Test
+    void getUserById() {
+    }
+}
