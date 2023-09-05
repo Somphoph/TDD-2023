bb6cc5405ee8c577c244f58d6d9736e1d06467bf
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-05 15:53:05 +0700

    Create test with wiremock load json file

--- api/src/main/java/com/example/api/user/UserGateway.java
+++ api/src/main/java/com/example/api/user/UserGateway.java
@@ -2,6 +2,7 @@ package com.example.api.user;
 
 import com.example.api.user.gateway.UserGatewayResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -12,14 +13,17 @@ import java.util.Optional;
 public class UserGateway {
     @Autowired
     private RestTemplate restTemplate;
-
+    @Value("${target_url}")
+    private String targetUrl;
     public Optional<UserGatewayResponse> callApi(int id) {
         try {
             UserGatewayResponse result = restTemplate.getForObject(
-                    "https://jsonplaceholder.typicode.com/users/" + id,
+//                    "https://jsonplaceholder.typicode.com/users/" + id,
+                    targetUrl+"/users/" + id,
                     UserGatewayResponse.class);
             return Optional.ofNullable(result);
         } catch (RestClientException e) {
+            e.printStackTrace();
             return Optional.empty();
         }
     }
