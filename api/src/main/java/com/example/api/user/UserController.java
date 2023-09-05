3fac420a3fd294e1ae67d1bd5db4f47ead96c565
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-05 11:37:45 +0700

    Split business logic to service layer

--- api/src/main/java/com/example/api/user/UserController.java
+++ api/src/main/java/com/example/api/user/UserController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/{id}")
     //GET /user/1
     //http://localhost:8080/user/1
     public UserResponse getUserById(@PathVariable int id){
-        UserResponse userResponse = new UserResponse();
-        userResponse.setId(id);
-        userResponse.setFirtname("Somkiat");
-        userResponse.setLastname("Pui");
-        return userResponse;
+        return userService.getById(id);
     }
 }
