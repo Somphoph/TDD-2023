a6aa2e5f624d7a027bd2fa80a2dd4698bf66f487
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-05 16:35:27 +0700

    Integrate UserGateway into UserService for API calls

--- api/src/main/java/com/example/api/user/UserService.java
+++ api/src/main/java/com/example/api/user/UserService.java
@@ -1,9 +1,16 @@
 package com.example.api.user;
 
+import com.example.api.user.gateway.UserGatewayResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserGateway userGateway;
+
     /**
      * 200 => Success
      * 404 => User not found , UserNotFoundException
@@ -12,11 +19,20 @@ public class UserService {
      * @param id
      * @return
      */
-    public UserResponse getById(int id) {
+    public UserResponse getById(int id){
+        // Get data from API
+        Optional<UserGatewayResponse> responseFromApi = userGateway.callApi(id);
+        if (!responseFromApi.isPresent()) {
+            throw new UserNotFoundException("User id=" + id + " not found in our system");
+        }
+
+        // Response from API
+        UserGatewayResponse response = responseFromApi.get();
+        // Create response to controller
         UserResponse result = new UserResponse();
-        result.setId(id);
-        result.setFirstname("Somkiat");
-        result.setLastname("Pui");
+        result.setId(response.getId());
+        result.setFirstname(response.getName());
+        result.setLastname(response.getUsername());
         return result;
     }
 }
