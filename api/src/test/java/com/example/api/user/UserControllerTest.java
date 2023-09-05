604020f65cc6573f05e4f43e26e7390c653fbc84
Somphop Homprasert <somphop.homprasert@krungthai.com>
2023-09-05 16:34:54 +0700

    Added WireMock and @Tag annotation to test classes

--- api/src/test/java/com/example/api/user/UserControllerTest.java
+++ api/src/test/java/com/example/api/user/UserControllerTest.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.nio.file.Files;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 9999)
 class UserControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
@@ -18,16 +26,31 @@ class UserControllerTest {
     @DisplayName("Success with get user information by id=1")
     void getUserById() {
         // Arrange
+        String jsonFromFile = readFromFile("classpath:user_1.json");
+        stubFor(get(urlEqualTo("/users/1")).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBody(jsonFromFile)));
+
         UserResponse expected = new UserResponse();
         expected.setId(1);
-        expected.setFirstname("Somkiat");
-        expected.setLastname("Pui");
+        expected.setFirstname("Mock name");
+        expected.setLastname("Mock username");
         //Act
         UserResponse result = restTemplate.getForObject("/user/1", UserResponse.class);
         //Assert
         assertEquals(1, result.getId());
-        assertEquals("Somkiat", result.getFirstname());
-        assertEquals("Pui", result.getLastname());
+        assertEquals("Mock name", result.getFirstname());
+        assertEquals("Mock username", result.getLastname());
         assertEquals(expected, result);
     }
+    private String readFromFile(String filename) {
+        try {
+            File file = ResourceUtils.getFile(filename);
+            byte[] data = Files.readAllBytes(file.toPath());
+            return new String(data);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
 }
